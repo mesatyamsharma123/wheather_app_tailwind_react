b5c77a90579b436ff8658d88d58c14f1b1678fc0
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-03-31 20:40:25 +0530

    done

--- wheatherapp/src/components/wheather/Home.jsx
+++ wheatherapp/src/components/wheather/Home.jsx
@@ -35,10 +35,10 @@ const input=useRef();
   }, []);
 
   return (
-    <div className="flex flex-col h-full items-center  mt-20 md:mt-30  ">
+    <div className="flex flex-col h-full items-center  mt-10 md:mt-30 mr-5 ml-5 ">
 <div
-  className="flex flex-col object-fit p-4 bg-slate-500 bg-cover bg-center rounded-2xl md:w-lg md:p-10"
-  style={{ backgroundImage: dark ? `url(${bgLight})` : `url(${bgDark})` }}
+  className="flex flex-col object-fit p-2 bg-slate-500 bg-cover bg-center rounded-2xl md:w-lg md:p-10"
+  style={{ backgroundImage: dark ? `url(${bgDark})` : `url(${bgLight})` }}
 >
         <div className="flex flex-row  gap-3">
           <input ref={input}
