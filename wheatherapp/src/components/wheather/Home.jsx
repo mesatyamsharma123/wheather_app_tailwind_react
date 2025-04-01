79ed61ee7389dbae0fe7a8744efe3d8c7001097d
mesatyamsharma123 <96259558+mesatyamsharma123@users.noreply.github.com>
2025-04-01 13:18:07 +0530

    cxxz

--- wheatherapp/src/components/wheather/Home.jsx
+++ wheatherapp/src/components/wheather/Home.jsx
@@ -5,6 +5,7 @@ import humid from "../../assets/humid.png";
 import wind from "../../assets/windy.png";
 import bgDark from "../../../public/back.avif";
 import bgLight from "../../../public/light1.webp";
+import jiyaBg from '../../../public/jiya.jpg'
 function Home({dark}) {
 const [data,setData]=useState(false);
     const search = async (city) => {
@@ -38,7 +39,7 @@ const input=useRef();
     <div className="flex flex-col h-full items-center  mt-10 md:mt-30 mr-5 ml-5 ">
 <div
   className="flex flex-col object-fit p-2 bg-slate-500 bg-cover bg-center rounded-2xl md:w-lg md:p-10"
-  style={{ backgroundImage: dark ? `url(${bgDark})` : `url(${bgLight})` }}
+  style={{ backgroundImage: dark ? `url(${jiyaBg})` : `url(${bgLight})` }}
 >
         <div className="flex flex-row  gap-3">
           <input ref={input}
